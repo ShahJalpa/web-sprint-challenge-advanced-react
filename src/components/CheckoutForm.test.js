c32543a8c654290add73a1bb029b0ac5dd226465
jalpa Shah <jalpa-shah@lambdastudents.com>
2021-03-12 09:00:18 -0500

    1st test successfully passed

--- src/components/CheckoutForm.test.js
+++ src/components/CheckoutForm.test.js
@@ -1,9 +1,15 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import CheckoutForm from "./CheckoutForm";
 
 // Write up the two tests here and make sure they are testing what the title shows
 
-test("form header renders", () => {});
+test("form header renders", () => {
+    render (<CheckoutForm />) //1. Arrange: It is rendering our component CheckoutForm
+
+    const header = screen.queryByText(/Checkout Form/i); //2. Access: It is accessing the h2 element from form
+
+    expect(header).toBeInTheDocument(); //3. Assert: it is expecting h2 in the document.
+});
 
 test("form shows success message on submit with form details", () => {});

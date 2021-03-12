f18fbeff30a39419e14da0fd8f19fbe69b0e4405
jalpa Shah <jalpa-shah@lambdastudents.com>
2021-03-12 08:30:09 -0500

    plantList class component is sucessfully showing the all plants

--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -8,6 +8,23 @@ export default class PlantList extends Component {
   //   - fetch data from the server endpoint - http://localhost:3333/plants
   //   - set the returned plants array to this.state.plants
 
+  constructor(){
+    super();
+    this.state = {plants: []};
+  };
+
+  componentDidMount(){
+    axios
+       .get('http://localhost:3333/plants')
+       .then((response) => {
+         //console.log(response);
+         this.setState({plants: response.data})
+       })
+       .catch((error) => {
+         console.log("error")
+       })
+  }
+
   /*********  DON'T CHANGE ANYTHING IN THE RENDER FUNCTION *********/
   render() {
     return (
